6d03ebbae6f329daa780d0a66bf888df6627cf50
Zhuo Chen <chen519@usc.edu>
2018-05-24 13:37:12 -0700

    01-05

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const { render } = ReactDOM
+import { render } from 'react-dom'
+import  React  from 'react'
+
+window.React = React
 
 render(
 	<h1 id='title'
 		className='header'
 		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-	HEY WORLD!!	
+	HEY WORLD!!
 	</h1>,
 	document.getElementById('react-container')
 )
